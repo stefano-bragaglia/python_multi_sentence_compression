566ede1827536460b3acbd1c5e7a5e72a92672ee
Stefano Bragaglia <stefano.bragaglia@astrazeneca.com>
2020-07-31 18:26:20 +0100

    Cleaning fter me

--- .autoenv.zsh
+++ .autoenv.zsh
@@ -1,10 +1,10 @@
 autostash NEO4J_CONF=$(pwd)
 autostash NEO4J_PASS='password'
 if [[ $autoenv_event == 'enter' ]]; then
-    echo dbms.directories.data=$(pwd)/neo4j/data > $(pwd)/neo4j.conf
-    echo dbms.directories.plugins=$(pwd)/neo4j/plugins >> $(pwd)/neo4j.conf
-    echo dbms.directories.import=$(pwd)/src/main/data/processed >> $(pwd)/neo4j.conf
-    echo 'dbms.security.procedures.unrestricted=apoc.*' >> $(pwd)/neo4j.conf
+#    echo dbms.directories.data=$(pwd)/neo4j/data > $(pwd)/neo4j.conf
+#    echo dbms.directories.plugins=$(pwd)/neo4j/plugins >> $(pwd)/neo4j.conf
+#    echo dbms.directories.import=$(pwd)/src/main/data/processed >> $(pwd)/neo4j.conf
+#    echo 'dbms.security.procedures.unrestricted=apoc.*' >> $(pwd)/neo4j.conf
     if [[ -a .env/bin/activate ]]; then
         source .env/bin/activate
     elif [[ -a .venv/bin/activate ]]; then
